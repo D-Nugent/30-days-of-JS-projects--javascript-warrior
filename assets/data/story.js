6cb0cacd772846a299a3590a19b233f0dace2043
David Nugent <davienugent@hotmail.com>
2021-07-31 18:53:25 -0700

    initial commit

---
+++ assets/data/story.js
@@ -0,0 +1,18 @@
+const storyDetails = [
+    {
+      id: 0,
+      description: "Our story begins on a beautiful sunny day🌄 in the kingdom of <span class='arr-emph'>[arr]</span>chadia. The mighty knight <span class='arr-emph'>[arr]</span>thur was escorting a royal c<span class='arr-emph'>[arr]</span>iage holding food🍗, supplies📦 and gold💰 to help a nearby town. While all was calm there was a feeling of dis-h<span class='arr-emph'>[arr]</span>mony in the air that troubled our hero..."
+    },
+    {
+      id: 1,
+      description: "A recent scroll📜 had reported of strange travellers in the <span class='arr-emph'>[arr]</span>ea - some who did not take kindly to the mon<span class='arr-emph'>[arr]</span>chy👑. It had been some time since <span class='arr-emph'>[arr]</span>thur had mastered and used the fighting <span class='arr-emph'>[arr]</span>t of array methods - he anticipated it wouldn't be long before he would need to take up <span class='arr-emph'>[arr]</span>ms against a new threat💪."
+    },
+    {
+      id: 2,
+      description: "Suddenly the weath<span class='arr-emph'>[arr]</span> changed and there was a great thunderclap ⚡ in the direction of the Castle🏰. Our hero jumped into action and made haste to the Castle knowing that princess <span class='arr-emph'>[arr]</span>iana 👸 was in danger."
+    },
+    {
+      id: 3,
+      description: "<span class='arr-emph'>[arr]</span>iving at the Castle gates just in time. <span class='arr-emph'>[arr]</span>thur stumbled upon the princess' stew<span class='arr-emph'>[arr]</span>d, the maiden m<span class='arr-emph'>[arr]</span>ia🧝‍♀️. With a tremble in her voice she proclaimed 'The D<span class='arr-emph'>[arr]</span>k Wiz<span class='arr-emph'>[arr]</span>d has returned!'. Ready to face the enemy and his <span class='arr-emph'>[arr]</span>my of monst<span class='arr-emph'>[arr]</span>s, our hero sets out to the battlefield!⚔"
+    }
+  ]
